abf8839e46600dda1fd66dec7876ae6a58c9787a
Valentin <valentin@kreatys.fr>
2021-06-28 15:50:54 +0200

    Added Golden Master

--- src/test/java/com/magasin/MagasinTest.java
+++ src/test/java/com/magasin/MagasinTest.java
@@ -7,11 +7,29 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 class MagasinTest {
 
     @Test
-    void foo() {
-        Item[] items = new Item[] { new Item("foo", 0, 0) };
+    void testItems() {
+        Item[] items = new Item[] {
+                new Item("Foo", 1, 0),
+                new Item("Normal", 0, 34),
+                new Item("Normal", 1, 2),
+                new Item("Kryptonite", 0, 80),
+                new Item("Pass VIP Concert", 4, 30),
+                new Item("Pass VIP Concert", 1, 3),
+                new Item("Comté", 4, 30),
+                new Item("Comté", 1, 1),
+
+        };
         Magasin app = new Magasin(items);
         app.updateQuality();
-        assertEquals("fixme", app.items[0].name);
+        app.updateQuality();
+
+        StringBuilder actual = new StringBuilder();
+        for(Item item : items){
+            actual.append(item).append(" - ");
+        }
+
+        System.out.println(actual);
+        assertEquals("Foo, -1, 0 - Normal, -2, 30 - Normal, -1, 0 - Kryptonite, 0, 80 - Pass VIP Concert, 2, 36 - Pass VIP Concert, -1, 0 - Comté, 2, 32 - Comté, -1, 4 - ",actual.toString());
     }
 
 }
