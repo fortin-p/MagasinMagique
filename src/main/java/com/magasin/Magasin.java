67c877a7c2a68b05ee3e35d19656c692c32bdb71
fortin-p <fortin.p@hotmail.fr>
2021-06-29 11:29:59 +0200

    Add github action

--- src/main/java/com/magasin/Magasin.java
+++ src/main/java/com/magasin/Magasin.java
@@ -7,58 +7,6 @@ class Magasin {
         this.items = items;
     }
 
-    public void updateQualityold() {
-        for (Item item : items) {
-            if (!item.name.equals("Comté")
-                    && !item.name.equals("Pass VIP Concert")) {
-                if (item.quality > 0) {
-                    if (!item.name.equals("Kryptonite")) {
-                        item.quality = item.quality - 1;
-                    }
-                }
-            } else {
-                if (item.quality < 50) {
-                    item.quality = item.quality + 1;
-
-                    if (item.name.equals("Pass VIP Concert")) {
-                        if (item.sellIn < 11) {
-                            if (item.quality < 50) {
-                                item.quality = item.quality + 1;
-                            }
-                        }
-
-                        if (item.sellIn < 6) {
-                            if (item.quality < 50) {
-                                item.quality = item.quality + 1;
-                            }
-                        }
-                    }
-                }
-            }
-
-            if (!item.name.equals("Kryptonite")) {
-                item.sellIn = item.sellIn - 1;
-            }
-
-            if (item.sellIn < 0) {
-                if (!item.name.equals("Comté")) {
-                    if (!item.name.equals("Pass VIP Concert")) {
-                        if (item.quality > 0) {
-                            if (!item.name.equals("Kryptonite")) {
-                                item.quality = item.quality - 1;
-                            }
-                        }
-                    } else {
-                        item.quality = 0;
-                    }
-                } else {
-                    if (item.quality < 50) {
-                        item.quality = item.quality + 1;
-                    }
-                }
-            }
-        }
-    }
 
     public void updateQuality() {
         for (Item item : items) {
