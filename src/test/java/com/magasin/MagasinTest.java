f33c676d6c58918d16f5f2e8461d9dd3818742c0
Valentin <valentin@kreatys.fr>
2021-06-29 10:30:49 +0200

    Fix Golden Master

--- src/test/java/com/magasin/MagasinTest.java
+++ src/test/java/com/magasin/MagasinTest.java
@@ -28,8 +28,7 @@ class MagasinTest {
             actual.append(item).append(" - ");
         }
 
-        System.out.println(actual);
-        assertEquals("Foo, -1, 0 - Normal, -2, 30 - Normal, -1, 0 - Kryptonite, 0, 80 - Pass VIP Concert, 2, 36 - Pass VIP Concert, -1, 0 - Comté, 2, 32 - Comté, -1, 4 - ",actual.toString());
+        assertEquals("Foo, -1, 0 - Normal, -2, 30 - Normal, -1, 0 - Kryptonite, 0, 80 - Pass VIP Concert, 2, 36 - Pass VIP Concert, -1, 0 - Comté, 2, 32 - Comté, -1, 3 - ",actual.toString());
     }
 
 }
