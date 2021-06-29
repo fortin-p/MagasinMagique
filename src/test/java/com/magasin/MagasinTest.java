0d3cbcf545b7c092bfc5562d3cae5468e2d24646
Valentin <valentin@kreatys.fr>
2021-06-29 10:30:01 +0200

    Fix Golden Master and refactor updateQuality

--- src/test/java/com/magasin/MagasinTest.java
+++ src/test/java/com/magasin/MagasinTest.java
@@ -29,7 +29,7 @@ class MagasinTest {
         }
 
         System.out.println(actual);
-        assertEquals("Foo, -1, 0 - Normal, -2, 30 - Normal, -1, 0 - Kryptonite, 0, 80 - Pass VIP Concert, 2, 36 - Pass VIP Concert, -1, 0 - Comté, 2, 32 - Comté, -1, 4 - ",actual.toString());
+        assertEquals("Foo, -1, 0 - Normal, -2, 30 - Normal, -1, 0 - Kryptonite, 0, 80 - Pass VIP Concert, 2, 36 - Pass VIP Concert, -1, 0 - Comté, 2, 32 - Comté, -1, 3 - ",actual.toString());
     }
 
 }
